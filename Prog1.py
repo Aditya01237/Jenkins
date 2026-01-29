3c0b0d07c668f5c454a2a44decceff1048fc1bd3
ADITYA PAREEK <80575917+Aditya01237@users.noreply.github.com>
2026-01-29 10:13:15 +0530

    Add summation program to calculate sum of numbers

---
+++ Prog1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Program to add two numbers
+
+def summation(data):
+    return sum(data)
+
+if __name__ == "__main__":
+    numbers = [10, 20]
+    print("Sum:", summation(numbers))
